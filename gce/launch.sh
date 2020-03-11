5b5babaaed230b32f3498149afa64d80eb10ee60
MrFlynn <nick@pleatsikas.me>
2020-03-11 15:09:15 -0700

    Revert "Remove firebase-config.json from env config."
    This reverts commit 2f980d54e1b22e0cbe1aacbbe80cd7fa6014a3b9.

--- gce/launch.sh
+++ gce/launch.sh
@@ -4,6 +4,15 @@
 # 
 # Maintainer: Nick Pleatsikas <nick@pleatsikas.me>
 
+cat <<EOF > ~/firebase-config.json
+{
+  "projectId": "opa-268409",
+  "databaseURL": "https://opa-268409.firebaseio.com/"
+}
+EOF
+
+export FIREBASE_CONFIG=~/firebase-config.json
+
 git clone https://github.com/opa-social/infrastructure.git
 cd infrastructure/gce || return
 
